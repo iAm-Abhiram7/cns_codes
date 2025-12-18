9a2666b94d96e5bbf6567ebe430cc8532665bd25
abhiram <contact.abhiram7@gmail.com>
2025-12-18 21:54:04 +0530

    Refactor RSAExample for improved encryption/decryption

--- lab2/RSAExample.java
+++ lab2/RSAExample.java
@@ -1,56 +1,34 @@
-package lab2;
-
+import java.util.*;
+import javax.crypto.*;
 import java.security.*;
-import javax.crypto.Cipher;
-import java.security.spec.*;
-
-public class RSAExample {
-
-    public static void main(String[] args) {
-        try {
-            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-            keyPairGenerator.initialize(2048);
-            KeyPair keyPair = keyPairGenerator.generateKeyPair();
-            PublicKey publicKey = keyPair.getPublic();
-            PrivateKey privateKey = keyPair.getPrivate();
-
-            printKeyDetails(publicKey, privateKey);
-
-            String plaintext = "Hello, RSA!";
-            System.out.println("Original Text: " + plaintext);
-
-            byte[] encryptedText = encrypt(plaintext, publicKey);
-            System.out.println("Encrypted Text: " + new String(encryptedText));
-
-            String decryptedText = decrypt(encryptedText, privateKey);
-            System.out.println("Decrypted Text: " + decryptedText);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+import java.security.interfaces.*;
+public class _1j_RSA {
+    public static String encrypt(PublicKey pk, String str) throws Exception{
+        Cipher c = Cipher.getInstance("RSA");
+        c.init(1, pk);
+        return new String(Base64.getEncoder().encode(c.doFinal(str.getBytes())));
     }
-
-    public static byte[] encrypt(String plaintext, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        return cipher.doFinal(plaintext.getBytes());
+    public static String decrypt(PrivateKey pk, String str) throws Exception{
+        Cipher c = Cipher.getInstance("RSA");
+        c.init(2, pk);
+        return new String(c.doFinal(Base64.getDecoder().decode(str.getBytes())));
     }
-
-    public static String decrypt(byte[] ciphertext, PrivateKey privateKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] decryptedBytes = cipher.doFinal(ciphertext);
-        return new String(decryptedBytes);
+    public static void main(String[] args) throws Exception{
+        KeyPairGenerator kg = KeyPairGenerator.getInstance("RSA");
+        kg.initialize(2048);
+        KeyPair kp = kg.generateKeyPair();
+        PublicKey pubK = kp.getPublic();
+        PrivateKey privK = kp.getPrivate();
+        printKeyDetails(pubK, privK);
+        String s = "hellp";
+        String enc = encrypt(pubK, s);
+        System.out.println(enc);
+        System.out.println(decrypt(privK, enc));
     }
-
-    public static void printKeyDetails(PublicKey publicKey, PrivateKey privateKey) throws Exception {
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        RSAPublicKeySpec publicKeySpec = keyFactory.getKeySpec(publicKey, RSAPublicKeySpec.class);
-        RSAPrivateKeySpec privateKeySpec = keyFactory.getKeySpec(privateKey, RSAPrivateKeySpec.class);
-
-        System.out.println("Public Key Modulus: " + publicKeySpec.getModulus());
-        System.out.println("Public Key Exponent: " + publicKeySpec.getPublicExponent());
-        System.out.println("Private Key Modulus: " + privateKeySpec.getModulus());
-        System.out.println("Private Key Exponent: " + privateKeySpec.getPrivateExponent());
+    public static void printKeyDetails(PublicKey pubK, PrivateKey privK){
+        RSAPublicKey rPub = (RSAPublicKey) pubK;
+        RSAPrivateKey rPriv = (RSAPrivateKey) privK;
+        System.out.println("public modulus : " + rPub.getModulus() + "public exponent : " + rPub.getPublicExponent());
+        System.out.println("private modulus : " + rPriv.getModulus() + "private exponent : " + rPriv.getPrivateExponent());
     }
 }
