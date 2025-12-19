20d1ab5ad4fffb44b6355c6a4e49e01a413a3ec2
abhiram <contact.abhiram7@gmail.com>
2025-12-19 06:47:03 +0530

    Refactor DESDemo class to improve encryption methods

--- DESDemo.java
+++ DESDemo.java
@@ -1,39 +1,32 @@
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import java.util.Base64;
-
-public class DESDemo {
-    public static SecretKey genKey() throws Exception {
+import java.util.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+public class _1f_DES {
+    public static SecretKey generateSecretKey() throws Exception{
         KeyGenerator kg = KeyGenerator.getInstance("DES");
         kg.init(56);
         return kg.generateKey();
     }
-
-    public static String enc(String pt, SecretKey key) throws Exception {
+    public static String encrypt(SecretKey sk, String str) throws Exception{
         Cipher c = Cipher.getInstance("DES");
-        c.init(Cipher.ENCRYPT_MODE, key);
-        byte[] out = c.doFinal(pt.getBytes());
-        return Base64.getEncoder().encodeToString(out);
+        c.init(Cipher.ENCRYPT_MODE, sk);
+        return Base64.getEncoder().encodeToString(c.doFinal(str.getBytes()));
     }
-
-    public static String dec(String ct, SecretKey key) throws Exception {
+    public static String decrypt(SecretKey sk, String encStr) throws Exception{
         Cipher c = Cipher.getInstance("DES");
-        c.init(Cipher.DECRYPT_MODE, key);
-        byte[] out = c.doFinal(Base64.getDecoder().decode(ct));
-        return new String(out);
+        c.init(Cipher.DECRYPT_MODE, sk);
+        return new String(c.doFinal(Base64.getDecoder().decode(encStr.getBytes())));
     }
-
     public static void main(String[] args) {
-        try {
-            SecretKey k = genKey();
-            String pt = "Hello, World!";
-            System.out.println("Original Text: " + pt);
-            String ct = enc(pt, k);
-            System.out.println("Encrypted Text: " + ct);
-            String dt = dec(ct, k);
-            System.out.println("Decrypted Text: " + dt);
-        } catch (Exception e) {
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println("Enter key size : ");
+        try{
+            SecretKey sk = generateSecretKey();
+            String enc = encrypt(sk, st);
+            System.out.println("Encrptted : " + enc);
+            System.out.println("Decrypted : " + decrypt(sk, enc));
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
